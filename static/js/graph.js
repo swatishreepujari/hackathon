b931e9c6d36e59acb82ecc46c66cbbb8a9e7f4b7
swatishree <pujari.swatishree@gmail.com>
2019-04-28 21:35:03 +0530

    updated 20190428

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,200 +1,3 @@
-function dashboard(id, fData){
-    var barColor = 'steelblue';
-    function segColor(c){ return {low:"#807dba", mid:"#e08214",high:"#41ab5d"}[c]; }
-
-    // compute total for each state.
-    fData.forEach(function(d){d.total=d.freq.low+d.freq.mid+d.freq.high;});
-
-    // function to handle histogram.
-    function histoGram(fD){
-        var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
-        hGDim.w = 500 - hGDim.l - hGDim.r,
-        hGDim.h = 200 - hGDim.t - hGDim.b;
-
-        //create svg for histogram.
-        var hGsvg = d3.select(id).append("svg")
-            .attr("width", hGDim.w + hGDim.l + hGDim.r)
-            .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
-            .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
-
-        // create function for x-axis mapping.
-        var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
-                .domain(fD.map(function(d) { return d[0]; }));
-
-        // Add x-axis to the histogram svg.
-        hGsvg.append("g").attr("class", "x axis")
-            .attr("transform", "translate(0," + hGDim.h + ")")
-            .call(d3.svg.axis().scale(x).orient("bottom"));
-
-        // Create function for y-axis map.
-        var y = d3.scale.linear().range([hGDim.h, 0])
-                .domain([0, d3.max(fD, function(d) { return d[1]; })]);
-
-        // Create bars for histogram to contain rectangles and freq labels.
-        var bars = hGsvg.selectAll(".bar").data(fD).enter()
-                .append("g").attr("class", "bar");
-
-        //create the rectangles.
-        bars.append("rect")
-            .attr("x", function(d) { return x(d[0]); })
-            .attr("y", function(d) { return y(d[1]); })
-            .attr("width", x.rangeBand())
-            .attr("height", function(d) { return hGDim.h - y(d[1]); })
-            .attr('fill',barColor)
-            .on("mouseover",mouseover)// mouseover is defined below.
-            .on("mouseout",mouseout);// mouseout is defined below.
-
-        //Create the frequency labels above the rectangles.
-        bars.append("text").text(function(d){ return d3.format(",")(d[1])})
-            .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
-            .attr("y", function(d) { return y(d[1])-5; })
-            .attr("text-anchor", "middle");
-
-        function mouseover(d){  // utility function to be called on mouseover.
-            // filter for selected state.
-            var st = fData.filter(function(s){ return s.State == d[0];})[0],
-                nD = d3.keys(st.freq).map(function(s){ return {type:s, freq:st.freq[s]};});
-
-            // call update functions of pie-chart and legend.
-            pC.update(nD);
-            leg.update(nD);
-        }
-
-        function mouseout(d){    // utility function to be called on mouseout.
-            // reset the pie-chart and legend.
-            pC.update(tF);
-            leg.update(tF);
-        }
-
-        // create function to update the bars. This will be used by pie-chart.
-        hG.update = function(nD, color){
-            // update the domain of the y-axis map to reflect change in frequencies.
-            y.domain([0, d3.max(nD, function(d) { return d[1]; })]);
-
-            // Attach the new data to the bars.
-            var bars = hGsvg.selectAll(".bar").data(nD);
-
-            // transition the height and color of rectangles.
-            bars.select("rect").transition().duration(500)
-                .attr("y", function(d) {return y(d[1]); })
-                .attr("height", function(d) { return hGDim.h - y(d[1]); })
-                .attr("fill", color);
-
-            // transition the frequency labels location and change value.
-            bars.select("text").transition().duration(500)
-                .text(function(d){ return d3.format(",")(d[1])})
-                .attr("y", function(d) {return y(d[1])-5; });
-        }
-        return hG;
-    }
-
-    // function to handle pieChart.
-    function pieChart(pD){
-        var pC ={},    pieDim ={w:250, h: 250};
-        pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
-
-        // create svg for pie chart.
-        var piesvg = d3.select(id).append("svg")
-            .attr("width", pieDim.w).attr("height", pieDim.h).append("g")
-            .attr("transform", "translate("+pieDim.w/2+","+pieDim.h/2+")");
-
-        // create function to draw the arcs of the pie slices.
-        var arc = d3.svg.arc().outerRadius(pieDim.r - 10).innerRadius(0);
-
-        // create a function to compute the pie slice angles.
-        var pie = d3.layout.pie().sort(null).value(function(d) { return d.freq; });
-
-        // Draw the pie slices.
-        piesvg.selectAll("path").data(pie(pD)).enter().append("path").attr("d", arc)
-            .each(function(d) { this._current = d; })
-            .style("fill", function(d) { return segColor(d.data.type); })
-            .on("mouseover",mouseover).on("mouseout",mouseout);
-
-        // create function to update pie-chart. This will be used by histogram.
-        pC.update = function(nD){
-            piesvg.selectAll("path").data(pie(nD)).transition().duration(500)
-                .attrTween("d", arcTween);
-        }
-        // Utility function to be called on mouseover a pie slice.
-        function mouseover(d){
-            // call the update function of histogram with new data.
-            hG.update(fData.map(function(v){
-                return [v.State,v.freq[d.data.type]];}),segColor(d.data.type));
-        }
-        //Utility function to be called on mouseout a pie slice.
-        function mouseout(d){
-            // call the update function of histogram with all data.
-            hG.update(fData.map(function(v){
-                return [v.State,v.total];}), barColor);
-        }
-        // Animating the pie-slice requiring a custom function which specifies
-        // how the intermediate paths should be drawn.
-        function arcTween(a) {
-            var i = d3.interpolate(this._current, a);
-            this._current = i(0);
-            return function(t) { return arc(i(t));    };
-        }
-        return pC;
-    }
-
-    // function to handle legend.
-    function legend(lD){
-        var leg = {};
-
-        // create table for legend.
-        var legend = d3.select(id).append("table").attr('class','legend');
-
-        // create one row per segment.
-        var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
-
-        // create the first column for each segment.
-        tr.append("td").append("svg").attr("width", '10').attr("height", '10').append("rect")
-            .attr("width", '10').attr("height", '10')
-			.attr("fill",function(d){ return segColor(d.type); });
-
-        // create the second column for each segment.
-        tr.append("td").text(function(d){ return d.type;});
-
-        // create the third column for each segment.
-        tr.append("td").attr("class",'legendFreq')
-            .text(function(d){ return d3.format(",")(d.freq);});
-
-        // create the fourth column for each segment.
-        tr.append("td").attr("class",'legendPerc')
-            .text(function(d){ return getLegend(d,lD);});
-
-        // Utility function to be used to update the legend.
-        leg.update = function(nD){
-            // update the data attached to the row elements.
-            var l = legend.select("tbody").selectAll("tr").data(nD);
-
-            // update the frequencies.
-            l.select(".legendFreq").text(function(d){ return d3.format(",")(d.freq);});
-
-            // update the percentage column.
-            l.select(".legendPerc").text(function(d){ return getLegend(d,nD);});
-        }
-
-        function getLegend(d,aD){ // Utility function to compute percentage.
-            return d3.format("%")(d.freq/d3.sum(aD.map(function(v){ return v.freq; })));
-        }
-
-        return leg;
-    }
-
-    // calculate total frequency by segment for all state.
-    var tF = ['low','mid','high'].map(function(d){
-        return {type:d, freq: d3.sum(fData.map(function(t){ return t.freq[d];}))};
-    });
-
-    // calculate total frequency by state for all segment.
-    var sF = fData.map(function(d){return [d.State,d.total];});
-
-    var hG = histoGram(sF); // create the histogram.
-        //pC = pieChart(tF); // create the pie-chart.
-        //leg= legend(tF);  // create the legend.
-}
-
 function pieGraph(id, oData){
     var key = oData.key;
     var barColor = 'steelblue';
@@ -736,4 +539,199 @@ var	svg = d3.select(id)
 		.call(yAxis);
 	};
     draw(corDataList);
+}
+
+function regression(id, rData){
+
+    regDataList = {};
+    sentiCount={};
+
+    rData.forEach(function(d) {
+        d.forEach(function(d1){
+            count = 0;
+            if (d1.qt.d in sentiCount) {
+                count = sentiCount[d1.qt.d];
+                if (d1.score == 'pos'){
+//                    console.log("pos count" + count)
+                    count = count + 1;
+                } else {
+//                    console.log("neg count" + count)
+                    count = count - 1;
+                }
+                sentiCount[d1.qt.d] = count;
+            } else {
+                sentiCount[d1.qt.d] = count +1;
+            }
+        });
+    });
+    rData.forEach(function(d) {
+         d.forEach(function(d1){
+            var regData ={};
+            regData.Change = d1.qt.c - d1.qt.o;
+            regData.SentiScore = sentiCount[d1.qt.d]
+            regDataList[d1.qt.d] = regData
+         });
+    });
+    var height = 300;
+	var width = 600;
+	var margin = {top: 20, right:20, bottom: 50, left: 20};
+
+	// formatters for axis and labels
+	var decimalFormat = d3.format("0.2f");
+	var currencyFormat = d3.format("$0.2f");
+
+	var svg = d3.select(id)
+		.append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	svg.append("g")
+		.attr("class", "y axis");
+
+	svg.append("g")
+		.attr("class", "x axis");
+
+	var xScale = d3.scale.ordinal()
+		.rangeRoundBands([margin.left, width], .1);
+
+	var yScale = d3.scale.linear()
+		.range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom");
+
+	var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left");
+
+	 // define function
+    function draw(data) {
+        // extract the x labels for the axis and scale domain
+		var xLabels = data.map(function (d) { return d.SentiScore; })
+
+		xScale.domain(xLabels);
+		yScale.domain([0, Math.round(d3.max(data, function(d) { return parseFloat(d.Change); }))]);
+
+		var line = d3.svg.line()
+			.x(function(d) { return xScale(d.SentiScore); })
+			.y(function(d) { return yScale(d.Change); });
+
+		svg.append("path")
+			.datum(data)
+			.attr("class","line")
+			.attr("d", line);
+
+		svg.select(".x.axis")
+			.attr("transform", "translate(0," + (height) + ")")
+			.call(xAxis.tickValues(xLabels.filter(function(d, i) {
+				if (i % 12 == 0)
+					return d;
+				})))
+			.selectAll("text")
+			.style("text-anchor","end")
+			.attr("transform", function(d) {
+				return "rotate(-45)";
+			});
+
+		svg.select(".y.axis")
+			.attr("transform", "translate(" + (margin.left) + ",0)")
+			.call(yAxis.tickFormat(currencyFormat));
+
+		// chart title
+		svg.append("text")
+			.attr("x", (width + (margin.left + margin.right) )/ 2)
+			.attr("y", 0 + margin.top)
+			.attr("text-anchor", "middle")
+			.style("font-size", "16px")
+			.style("font-family", "sans-serif")
+			.text("USD/EURO Exhange Rate");
+
+		// x axis label
+		svg.append("text")
+			.attr("x", (width + (margin.left + margin.right) )/ 2)
+			.attr("y", height + margin.bottom)
+			.attr("class", "text-label")
+			.attr("text-anchor", "middle")
+			.text("Year-Month");
+
+		// get the x and y values for least squares
+		var xSeries = d3.range(1, xLabels.length + 1);
+		var ySeries = data.map(function(d) { return decimalFormat(d.Change); });
+
+		var leastSquaresCoeff = leastSquares(xSeries, ySeries);
+
+		// apply the reults of the least squares regression
+		var x1 = xLabels[0];
+		var y1 = leastSquaresCoeff[0] + leastSquaresCoeff[1];
+		var x2 = xLabels[xLabels.length - 1];
+		var y2 = leastSquaresCoeff[0] * xSeries.length + leastSquaresCoeff[1];
+		var trendData = [[x1,y1,x2,y2]];
+
+		var trendline = svg.selectAll(".trendline")
+			.data(trendData);
+
+		trendline.enter()
+			.append("line")
+			.attr("class", "trendline")
+			.attr("x1", function(d) { return xScale(d[0]); })
+			.attr("y1", function(d) { return yScale(d[1]); })
+			.attr("x2", function(d) { return xScale(d[2]); })
+			.attr("y2", function(d) { return yScale(d[3]); })
+			.attr("stroke", "black")
+			.attr("stroke-width", 1);
+
+		// display equation on the chart
+		svg.append("text")
+			.text("eq: " + decimalFormat(leastSquaresCoeff[0]) + "x + " +
+				decimalFormat(leastSquaresCoeff[1]))
+			.attr("class", "text-label")
+			.attr("x", function(d) {return xScale(x2) - 60;})
+			.attr("y", function(d) {return yScale(y2) - 30;});
+
+		// display r-square on the chart
+		svg.append("text")
+			.text("r-sq: " + decimalFormat(leastSquaresCoeff[2]))
+			.attr("class", "text-label")
+			.attr("x", function(d) {return xScale(x2) - 60;})
+			.attr("y", function(d) {return yScale(y2) - 10;});
+
+	};
+
+	// returns slope, intercept and r-square of the line
+	function leastSquares(xSeries, ySeries) {
+		var reduceSumFunc = function(prev, cur) { return prev + cur; };
+
+		var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+		var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+
+		var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+			.reduce(reduceSumFunc);
+
+		var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+			.reduce(reduceSumFunc);
+
+		var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+			.reduce(reduceSumFunc);
+
+		var slope = ssXY / ssXX;
+		var intercept = yBar - (xBar * slope);
+		var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+
+		return [slope, intercept, rSquare];
+
+    }
+
+     var values = Object.keys(regDataList).map(function(key){
+            return regDataList[key];
+        });
+      values.forEach(function(d) {
+        d.Change = d.Change;
+        d.SentiScore = d.SentiScore;
+     });
+     console.log(values);
+
+     draw(values)
+
 }
