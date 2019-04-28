ff520f766e958b28236c0e8c7cb7034d834c9222
swatishree <pujari.swatishree@gmail.com>
2019-04-28 16:59:18 +0530

    updated 20190428

--- static/js/graph.js
+++ static/js/graph.js
@@ -634,6 +634,7 @@ var	svg = d3.select(id)
 	// Add the valueline path.
 	svg.append("path")		// Add the valueline path.
 	    .style("stroke", "red")
+	    .stroke-width: 2px;
 	    .style("stroke-dasharray", ("3, 3"))
 		.attr("class", "line")
 		.attr("d", valueline(values));
